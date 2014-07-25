14e2b9e5439d42059ee157d6d544f3a2c210dd5d
dawidrichert@gmail.com <DimaKaru2@>
2014-07-25 16:13:55 +0200

    Repair styles problem.

--- app/src/main/java/com/example/dawidr/mysecondapp/DisplayMessageActivity.java
+++ app/src/main/java/com/example/dawidr/mysecondapp/DisplayMessageActivity.java
@@ -29,7 +29,6 @@ public class DisplayMessageActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.display_message, menu);
         return true;
-
     }
 
     @Override
